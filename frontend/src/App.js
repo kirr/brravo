c20c161254b99547c19cb81727775a1abb832538
Kirill Kosarev <kirr@yandex-team.ru>
2019-11-17 23:45:59 +0300

    Exercise screen back button.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {BottomNavigation, BottomNavigationAction} from '@material-ui/core';
-import {MenuBook, Assignment, Info} from '@material-ui/icons';
+import {BottomNavigation, BottomNavigationAction, IconButton} from '@material-ui/core';
+import {MenuBook, Assignment, Info, ArrowBack} from '@material-ui/icons';
 import {Lesson, Exercises, About} from './navigation_screens';
 import {AutomatitionExercise} from './exercises';
 
@@ -29,24 +29,48 @@ function ScreenById(screenId, exerciseChooseHandler) {
     }
 }
 
+function RenderBottomNavbar(screen, setScreenCallback) {
+  if (screen !== 'exercise') {
+    return (<AbsoluteButtomNavigation
+              value={screen}
+              onChange={(event, newValue) => {
+                      setScreenCallback(newValue);
+              }}
+              showLabels
+              classes={{label: BottomNavigation}}
+            >
+              <BottomNavigationAction value="lesson" icon={<MenuBook />} />
+              <BottomNavigationAction value="exercises"  icon={<Assignment />} />
+              <BottomNavigationAction value="about" icon={<Info />} />
+            </AbsoluteButtomNavigation>);
+  } else {
+    return null;
+  }
+}
+
+function RenderBackButton(screen, prevScreen, setScreenCallback) {
+  if (screen === 'exercise') {
+    return (<IconButton onClick={()=>{setScreenCallback(prevScreen);}}>
+              <ArrowBack />
+            </IconButton>);
+  } else {
+    return null;
+  }
+}
+
 function App() {
   const [screen, setScreen] = React.useState('lesson');
+  const prevScreenRef = React.useRef();
+  React.useEffect(() => {
+    prevScreenRef.current = screen;
+  });
+  const prevScreen = prevScreenRef.current;
   const screenElement = ScreenById(screen, ()=>{ setScreen('exercise'); });
   return (
     <div className="App">
+        {RenderBackButton(screen, prevScreen, setScreen)}
         {screenElement}
-        <AbsoluteButtomNavigation
-          value={screen}
-          onChange={(event, newValue) => {
-                  setScreen(newValue);
-          }}
-          showLabels
-          classes={{label: BottomNavigation}}
-        >
-          <BottomNavigationAction value="lesson" icon={<MenuBook />} />
-          <BottomNavigationAction value="exercises"  icon={<Assignment />} />
-          <BottomNavigationAction value="about" icon={<Info />} />
-        </AbsoluteButtomNavigation>
+        {RenderBottomNavbar(screen, setScreen)}
     </div>
   );
 }
