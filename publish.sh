206215a176fd5e2d973294333b03b11dc06f15c0
Kirill Kosarev <kirr@yandex-team.ru>
2019-11-20 14:50:27 +0300

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DOCKER_IMAGE=cr.yandex/crpb7dk0spagmsiikn2c/brravo:latest
+cd "$(dirname "$0")"
+(cd ./frontend && npm run build) && \
+    docker build -t $DOCKER_IMAGE -f Dockerfile . && \
+    docker push $DOCKER_IMAGE && \
+    yc compute instance update-container --name brravo --container-image $DOCKER_IMAGE
