ebf198acc4740a8598d125dd421b0eee37ac9816
Kirill Kosarev <kirr@yandex-team.ru>
2019-12-11 11:28:55 +0300

    Updated publish scripts

--- publish.sh
+++ publish.sh
@@ -3,5 +3,7 @@ DOCKER_IMAGE=cr.yandex/crpb7dk0spagmsiikn2c/brravo:latest
 cd "$(dirname "$0")"
 (cd ./frontend && npm run build) && \
     docker build -t $DOCKER_IMAGE -f Dockerfile . && \
-    docker push $DOCKER_IMAGE
+    docker push $DOCKER_IMAGE && \
+    scp server_update.sh kirr@84.201.173.241: && \
+    ssh -A 84.201.173.241 sudo ./server_update.sh
 # yc compute instance update-container --name brravo --container-image $DOCKER_IMAGE
